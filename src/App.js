e8072298b9f78a839403b40df16e9cfe3b1ed3ac
cineron <cineron+gh@gmail.com>
2018-10-07 17:58:28 -0500

    Adds new files for React components and installs React Router.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
 // import * as BooksAPI from './BooksAPI'
-import './App.css'
+import './App.css';
+
+// import React Router
+// import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
   state = {
