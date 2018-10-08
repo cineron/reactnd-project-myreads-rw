c19aa45eb6aa5a186f19a5b2862b2b7ad41ab141
cineron <cineron+gh@gmail.com>
2018-10-07 19:04:49 -0500

    Adds React Router Links to new SearchPage component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import React from 'react';
 import './App.css';
 
 // import React Router
-// import { Route } from "react-router-dom";
+import { Route } from "react-router-dom";
+// import MainPage component
+import MainPage from "./MainPage.js";
+// import SearchPage component
+import SearchPage from "./SearchPage.js";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
+    return (
+      <div>
+        <Route exact path="/" component={ MainPage }/>
+        <Route path="/search" component={ SearchPage }/>
+      </div>
+    )
+
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
