c0ed311d5eec5039d7a22b3229c6a692f3dbb78f
cineron <cineron+gh@gmail.com>
2018-10-08 18:59:39 -0500

    Fixes moving books with attached menu

--- src/Book.js
+++ src/Book.js
@@ -5,37 +5,15 @@ import * as BooksAPI from './BooksAPI';
 
 class Book extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-        book: props.book
-    }
-  }
-
-  componentDidMount(){
-    console.log(this);
-  }
-
-  updateBook(shelf){
-    BooksAPI.update(this.state.book, shelf)
-    .then(resp => {
-      this.setState(state => {
-        let copy = state.book;
-        copy.shelf = shelf;
-        this.setState({ book : copy });
-      } )
-    })
-  }
-
     render() {
         return(
             <li>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.state.book.imageLinks && this.state.book.imageLinks.thumbnail} || ''")` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail} || ''")` }}></div>
                 <div className="book-shelf-changer">
-                  <select value={this.state.book.shelf || 'none'} onChange= {(e) => {
-                    this.updateBook(e.target.value)
+                  <select value={this.props.book.shelf || 'none'} onChange= {(e) => {
+                    this.props.updateBook(this.props.book, e.target.value)
                     }}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
@@ -45,8 +23,8 @@ class Book extends React.Component {
                   </select>
                 </div>
               </div>
-              <div className="book-title">{this.state.book.title}t</div>
-              <div className="book-authors">{this.state.book.authors[0] || "No author on file."}</div>
+              <div className="book-title">{this.props.book.title}t</div>
+              <div className="book-authors">{this.props.book.authors[0] || "No author on file."}</div>
             </div>
           </li>
         )
--- src/MainPage.js
+++ src/MainPage.js
@@ -21,6 +21,16 @@ class MainPage extends React.Component {
         })
     }
 
+    updateBook = (book, shelf) => {
+        BooksAPI.update(book, shelf)
+        .then(resp => {
+            book.shelf = shelf;
+            this.setState(state => ({
+                books: state.books.filter(b => b.id !== book.id).concat([book])
+            }))
+        });
+    }
+
 
     render() {
         return(
@@ -29,9 +39,9 @@ class MainPage extends React.Component {
                 <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-                <Shelf name="currentlyReading" books={this.state.books.filter(b => b.shelf === "currentlyReading")} />
-                <Shelf name="Want To Read" books={this.state.books.filter(b => b.shelf === "wantToRead")} />
-                <Shelf name="Read" books={this.state.books.filter(b => b.shelf === "read")} />
+                <Shelf updateBook={this.updateBook} name="currentlyReading" books={this.state.books.filter(b => b.shelf === "currentlyReading")} />
+                <Shelf updateBook={this.updateBook} name="Want To Read" books={this.state.books.filter(b => b.shelf === "wantToRead")} />
+                <Shelf updateBook={this.updateBook} name="Read" books={this.state.books.filter(b => b.shelf === "read")} />
             </div>
             <div className="open-search">
                 {/* clean up links */}
