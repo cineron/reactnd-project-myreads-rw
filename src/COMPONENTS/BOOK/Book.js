3a141ada61191275209d04c9734f0b213b9d734a
cineron <cineron+github@gmail.com>
2020-08-15 00:47:51 -0500

    Removes unused imports and class components for functional componenets.

--- src/COMPONENTS/BOOK/Book.js
+++ src/COMPONENTS/BOOK/Book.js
@@ -1,36 +1,27 @@
 import React from "react";
-//import React Router Link
-import { Link } from "react-router-dom";
-import * as BooksAPI from '../../APIS/BooksAPI';
 
-// class Book extends React.Component {
 const Book = (props) => (
-    // render() {
-        // return(
-            <li>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${props.book.imageLinks && props.book.imageLinks.thumbnail} || ''")` }}>
-                </div>
-                <div className="book-shelf-changer">
-                  <select value={props.book.shelf || 'none'} onChange= {(e) => {
-                    props.updateBook(props.book, e.target.value)
-                    }}>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{props.book.title}</div>
-              <div className="book-authors">{(props.book.authors && props.book.authors[0]) || "(no author listed)"}</div>
+    <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${props.book.imageLinks && props.book.imageLinks.thumbnail} || ''")` }}>
             </div>
-          </li>
-        // )
-    // }
-// }
+            <div className="book-shelf-changer">
+              <select value={props.book.shelf || 'none'} onChange= {(e) => {
+                props.updateBook(props.book, e.target.value)
+                }}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{props.book.title}</div>
+          <div className="book-authors">{(props.book.authors && props.book.authors[0]) || "(no author listed)"}</div>
+        </div>
+    </li>
 )
 
 export default Book;
--- src/COMPONENTS/SHELF/Shelf.js
+++ src/COMPONENTS/SHELF/Shelf.js
@@ -1,28 +1,18 @@
 import React from "react";
-//import React Router Link
-import { Link } from "react-router-dom";
 import Book from "../BOOK";
 
-class Shelf extends React.Component {
-    // componentDidMount(){
-    //     console.log(this);
-    // }
-
-    render() {
-        return(
-        <div className="bookshelf">
-            <h2 className="bookshelf-title">{this.props.name}</h2>
-            <div className="bookshelf-books">
-                <ol className="books-grid">
-                    {this.props.books.map((book, key) => 
-                        <Book updateBook={this.props.updateBook} book={book} key={key} />
-                    )}
-                </ol>
-            </div>
+const Shelf = (props) => (
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.name}</h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+                {props.books.map((book, key) => 
+                    <Book updateBook={props.updateBook} book={book} key={key} />
+                )}
+            </ol>
         </div>
-        )
-    }
-}
+    </div>
+)
 
 export default Shelf;
 
